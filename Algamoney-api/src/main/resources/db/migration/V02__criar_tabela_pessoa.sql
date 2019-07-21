41e84ee7d71dba52904a658e86c4c51331eb13a3
Fernando Lucas <fernandolucassud@gmail.com>
2019-07-20 22:13:29 -0300

    Correção de complemento da tabela pessoa

--- Algamoney-api/src/main/resources/db/migration/V02__criar_tabela_pessoa.sql
+++ Algamoney-api/src/main/resources/db/migration/V02__criar_tabela_pessoa.sql
@@ -4,31 +4,32 @@ nome VARCHAR(100) NOT NULL,
 ativo BOOLEAN NOT NULL,
 logradouro VARCHAR(100),
 numero VARCHAR(20),
+complemento VARCHAR(30),
 bairro VARCHAR(50),
 cep VARCHAR(20),
 cidade VARCHAR(20),
 estado VARCHAR(50)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO pessoa (nome, ativo,logradouro, numero, bairro, cep, cidade, estado) 
-VALUES('Fernado',true,'Quadra 10 Conjunto L','5','setor sul','72415-512','Gama','Distrito Federal');
-INSERT INTO pessoa (nome, ativo,logradouro, numero, bairro, cep, cidade, estado) 
-VALUES('Fatima',true,'Quadra 10 Conjunto L','5','setor sul','72415-512','Gama','Distrito Federal');
-INSERT INTO pessoa (nome, ativo,logradouro, numero, bairro, cep, cidade, estado) 
-VALUES('João',true,'Quadra 10 Conjunto L','5','setor sul','72415-512','Gama','Distrito Federal');
-INSERT INTO pessoa (nome, ativo,logradouro, numero, bairro, cep, cidade, estado) 
-VALUES('Benjamin',true,'Quadra 10 Conjunto L','5','setor sul','72415-512','Gama','Distrito Federal');
-INSERT INTO pessoa (nome, ativo,logradouro, numero, bairro, cep, cidade, estado) 
-VALUES('Maria',true,'Quadra 09 Conjunto J','18','setor sul','72410-510','Gama','Distrito Federal');
-INSERT INTO pessoa (nome, ativo,logradouro, numero, bairro, cep, cidade, estado) 
-VALUES('Gabriel',true,'Quadra 09 Conjunto J','18','setor sul','72410-510','Gama','Distrito Federal');
-INSERT INTO pessoa (nome, ativo,logradouro, numero, bairro, cep, cidade, estado) 
-VALUES('Patricia',false,'Quadra 09 Conjunto J','18','setor sul','72410-510','Gama','Distrito Federal');
-INSERT INTO pessoa (nome, ativo,logradouro, numero, bairro, cep, cidade, estado) 
-VALUES('Vera',true,'Quadra 04 Conjunto C','1','setor sul','72415-512','Gama','Distrito Federal');
-INSERT INTO pessoa (nome, ativo,logradouro, numero, bairro, cep, cidade, estado) 
-VALUES('Rosimeire',true,'Quadra 04 Conjunto C','1','setor sul','72415-512','Gama','Distrito Federal');
-INSERT INTO pessoa (nome, ativo,logradouro, numero, bairro, cep, cidade, estado) 
-VALUES('Camila',true,'Quadra 04 Conjunto C','1','setor sul','72415-512','Gama','Distrito Federal');
+INSERT INTO pessoa (nome, ativo,logradouro, numero,complemento, bairro, cep, cidade, estado) 
+VALUES('Fernado',true,'Quadra 10 Conjunto L','5',null,'setor sul','72415-512','Gama','Distrito Federal');
+INSERT INTO pessoa (nome, ativo,logradouro, numero,complemento, bairro, cep, cidade, estado) 
+VALUES('Fatima',true,'Quadra 10 Conjunto L','5',null,'setor sul','72415-512','Gama','Distrito Federal');
+INSERT INTO pessoa (nome, ativo,logradouro, numero,complemento, bairro, cep, cidade, estado)  
+VALUES('João',true,'Quadra 10 Conjunto L','5',null,'setor sul','72415-512','Gama','Distrito Federal');
+INSERT INTO pessoa (nome, ativo,logradouro, numero,complemento, bairro, cep, cidade, estado) 
+VALUES('Benjamin',true,'Quadra 10 Conjunto L','5',null,'setor sul','72415-512','Gama','Distrito Federal');
+INSERT INTO pessoa (nome, ativo,logradouro, numero,complemento, bairro, cep, cidade, estado) 
+VALUES('Maria',true,'Quadra 09 Conjunto J','18',null,'setor sul','72410-510','Gama','Distrito Federal');
+INSERT INTO pessoa (nome, ativo,logradouro, numero,complemento, bairro, cep, cidade, estado) 
+VALUES('Gabriel',true,'Quadra 09 Conjunto J','18',null,'setor sul','72410-510','Gama','Distrito Federal');
+INSERT INTO pessoa (nome, ativo,logradouro, numero,complemento, bairro, cep, cidade, estado) 
+VALUES('Patricia',false,'Quadra 09 Conjunto J','18',null,'setor sul','72410-510','Gama','Distrito Federal');
+INSERT INTO pessoa (nome, ativo,logradouro, numero,complemento, bairro, cep, cidade, estado)  
+VALUES('Vera',true,'Quadra 04 Conjunto C','1',null,'setor sul','72415-512','Gama','Distrito Federal');
+INSERT INTO pessoa (nome, ativo,logradouro, numero,complemento, bairro, cep, cidade, estado)  
+VALUES('Rosimeire',true,'Quadra 04 Conjunto C','1',null,'setor sul','72415-512','Gama','Distrito Federal');
+INSERT INTO pessoa (nome, ativo,logradouro, numero,complemento, bairro, cep, cidade, estado) 
+VALUES('Camila',true,'Quadra 04 Conjunto C','1',null,'setor sul','72415-512','Gama','Distrito Federal');
 
 
