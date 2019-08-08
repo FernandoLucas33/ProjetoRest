8a0aa96a6c56f2253320f89ea32bcb63d79abace
Fernando Lucas <fernandolucassud@gmail.com>
2019-08-08 07:00:13 -0300

    CORS da aplicação funcionando.

--- Algamoney-api/src/main/java/com/example/algamoney/api/token/RefreshTokenCookieProcessorFilter.java
+++ Algamoney-api/src/main/java/com/example/algamoney/api/token/RefreshTokenCookieProcessorFilter.java
@@ -2,13 +2,13 @@ package com.example.algamoney.api.token;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 
@@ -29,12 +29,15 @@ public class RefreshTokenCookieProcessorFilter implements Filter {
 		if("/oauth/token".equalsIgnoreCase(req.getRequestURI())
 			&& "refresh_token".equals(req.getParameter("grant_type"))
 			&& req.getCookies() != null){
-				for (Cookie cookie : req.getCookies()) {
-					if (cookie.getName().equals("refreshToken")) {
-						String refreshToken = cookie.getValue();
-						req = new MyServletRequestWrapper(req, refreshToken);
-					}
-				}
+			
+				String refreshToken =
+						Stream.of(req.getCookies())
+						.filter(cookie -> "refreshToken".equals(cookie.getName()))
+						.findFirst()
+						.map(cookie -> cookie.getValue())
+						.orElse(null);
+				
+				req = new MyServletRequestWrapper(req, refreshToken);
 			}
 		chain.doFilter(req, response);
 	}
@@ -55,7 +58,7 @@ public class RefreshTokenCookieProcessorFilter implements Filter {
 			ParameterMap<String, String[]> map = new ParameterMap<>(getRequest().getParameterMap());
 			map.put("refresh_token", new String[] {refreshToken});
 			map.setLocked(true);
-			return super.getParameterMap();
+			return map;
 		}
 
 }
